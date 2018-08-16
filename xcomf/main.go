88b642e48abfb493b3498a208238bf149dc14452
Camille Scholtz <camille@airmail.cc>
2018-08-16 14:45:44 +0200

    xcomf rewrite

--- xcomf/main.go
+++ xcomf/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/BurntSushi/xgb/randr"
 	"github.com/BurntSushi/xgbutil"
 	"github.com/go2c/optparse"
+	"github.com/maruel/temperature"
 )
 
 func main() {
@@ -24,74 +25,105 @@ func main() {
 
 	// Print help.
 	if *argh {
-		fmt.Println("Usage: xcomf [arguments] [temperature]")
+		fmt.Println("Usage: xcomf [arguments] [intensity]")
 		fmt.Println("")
 		fmt.Println("arguments:")
 		fmt.Println("  -h,   --help            print help and exit")
 		os.Exit(0)
 	}
 
+	// Reset gamma to default value if no vals have been given.
+	if len(vals) == 0 {
+		vals = []string{"0"}
+	}
+
+	// Only alow intergers in vals.
+	i, err := strconv.Atoi(vals[0])
+	if err != nil || i < 0 || i > 3 {
+		fmt.Fprintln(os.Stderr,
+			"Please choose an intensity between 0 and 3!")
+		os.Exit(1)
+	}
+
+	var t uint16
+	switch i {
+	case 0:
+		t = 6500
+	case 1:
+		t = 4000
+	case 2:
+		t = 3500
+	case 3:
+		t = 3000
+	}
+
+	if err := set(temperature.ToRGB(t)); err != nil {
+		panic(err)
+	}
+}
+
+func gamma(r, g, b uint8, size uint16, comf bool) ([]uint16, []uint16,
+	[]uint16) {
+	gr := make([]uint16, size)
+	gg := make([]uint16, size)
+	gb := make([]uint16, size)
+
+	for i := uint16(0); i < size; i++ {
+		fr := float64(r) / 255
+		fg := float64(g) / 255
+		fb := float64(b) / 255
+		fs := float64(size)
+		if comf {
+			fb *= 1.618
+			fs *= 0.85
+			fs += float64(i) * 0.5
+		}
+
+		gamma := 65535 * float64(i) / fs
+
+		gr[i] = uint16(gamma * fr)
+		gg[i] = uint16(gamma * fg)
+		gb[i] = uint16(gamma * fb)
+	}
+
+	return gr, gg, gb
+}
+
+func set(r, g, b uint8) error {
 	// Connect to the X server using the DISPLAY environment variable.
 	X, err := xgbutil.NewConn()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// Initialize randr.
 	if err := randr.Init(X.Conn()); err != nil {
-		panic(err)
+		return err
 	}
 
-	sr, err := randr.GetScreenResources(X.Conn(), X.RootWin()).Reply()
+	sr, err := randr.GetScreenResourcesCurrent(X.Conn(), X.RootWin()).Reply()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	r, err := randr.GetCrtcGamma(X.Conn(), sr.Crtcs[0]).Reply()
-	if err != nil {
-		panic(err)
+	for _, c := range sr.Crtcs {
+		gs, err := randr.GetCrtcGammaSize(X.Conn(), c).Reply()
+		if err != nil {
+			return err
+		}
+
+		var gg, gr, gb []uint16
+		if r == 255 && g == 255 && b == 255 {
+			gr, gg, gb = gamma(r, g, b, gs.Size, false)
+		} else {
+			gr, gg, gb = gamma(r, g, b, gs.Size, true)
+		}
+
+		if err := randr.SetCrtcGammaChecked(X.Conn(), c, gs.Size, gr, gg, gb).
+			Check(); err != nil {
+			return err
+		}
 	}
 
-	// Reset gamma to default value if no vals have been given.
-	if len(vals) == 0 {
-		randr.SetCrtcGamma(X.Conn(), sr.Crtcs[0], r.Size, r.Red,
-			r.Green, r.Blue)
-		os.Exit(0)
-	}
-
-	// Only alow intergers in vals.
-	temp, err := strconv.Atoi(vals[0])
-	if err != nil || temp < 1 || temp > 10 {
-		fmt.Fprintln(os.Stderr,
-			"Please choose a temperature between 1 and 10!")
-		os.Exit(1)
-	}
-
-	var nr []uint16
-	for i := range r.Red {
-		v := int(r.Red[i])
-		v += int(r.Red[len(r.Red)-i-1]) / 8
-		v -= (len(r.Red) / 100 * (i * 2)) / 4
-		v = ((v * temp) + (int(r.Red[i]) * (10 - temp))) / 10
-
-		nr = append(nr, uint16(v))
-	}
-	var ng []uint16
-	for i := range r.Green {
-		v := int(r.Green[i])
-		v -= (len(r.Green) / 100 * (i * 2)) / 3
-		v = ((v * temp) + (int(r.Green[i]) * (10 - temp))) / 10
-
-		ng = append(ng, uint16(v))
-	}
-	var nb []uint16
-	for i := range r.Blue {
-		v := int(r.Blue[i])
-		v -= (len(r.Blue) / 100 * (i * 2)) / 3
-		v = ((v * temp) + (int(r.Blue[i]) * (10 - temp))) / 10
-
-		nb = append(nb, uint16(v))
-	}
-
-	randr.SetCrtcGamma(X.Conn(), sr.Crtcs[0], r.Size, nr, ng, nb)
+	return nil
 }
